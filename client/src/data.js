2941110b203b79cd654bae80bc36c7c0f585b013
Andrey <andreymatheus92@gmail.com>
2024-10-25 18:36:49 -0300

    feat: add post detail page

---
+++ client/src/data.js
@@ -0,0 +1,39 @@
+import Thumbnail1 from "./assets/blog1.jpg";
+import Thumbnail2 from "./assets/blog2.jpg";
+import Thumbnail3 from "./assets/blog3.jpg";
+import Thumbnail4 from "./assets/blog4.jpg";
+
+export const DUMMY_POSTS = [
+  {
+    id: "1",
+    thumbnail: Thumbnail1,
+    category: "education",
+    title: "This is a test title for the first post",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At quidem vitae consectetur. Impedit itaque placeat ipsam libero maxime repellendus cupiditate veniam deleniti, tenetur pariatur iste dolor accusamus fugit eligendi perferendis. Earum expedita facilis maiores doloribus commodi praesentium, placeat tempora dolores libero, eligendi, quibusdam odit officia unde quas explicabo sint nam ab cupiditate consectetur soluta sapiente eaque autem tenetur. Ab iusto optio cumque dicta exercitationem, alias fuga excepturi, dolore at temporibus blanditiis magnam magni fugiat dolorum minima deleniti tenetur quaerat ipsam amet! Minus ratione, quasi nisi praesentium iure eum perferendis quas consequatur modi id ex quo quia aliquam, asperiores numquam nobis odit reiciendis, architecto vero necessitatibus doloremque est esse harum nam? A impedit quo error eum consectetur facilis tenetur? Explicabo perspiciatis, labore, iure unde qui accusamus enim quia, debitis nihil laboriosam fugiat soluta ducimus voluptates temporibus? Laboriosam tempora, dolore, eum voluptatibus repellendus libero aliquam molestias neque nisi pariatur commodi quo consectetur nostrum laborum, saepe optio et atque molestiae ex sint? Quis ipsum perspiciatis minima, possimus reprehenderit sequi corporis qui harum eaque nihil labore sit incidunt ex quibusdam exercitationem veritatis modi, molestiae hic? Corrupti ratione magnam fugiat rem, temporibus molestias. Eaque sunt temporibus, tempore quam perferendis laboriosam recusandae! Autem eveniet eligendi, dolores nam recusandae, sit atque eaque maxime quidem illo expedita voluptates deserunt nemo error. Dolor libero maxime fuga voluptate necessitatibus placeat, veniam itaque debitis deserunt nisi repellat rerum, quo quasi ducimus ipsam. Rerum, omnis? Culpa ipsum accusantium delectus adipisci numquam quam, laborum excepturi facere. Voluptatem quos odit a molestias tenetur aliquam nemo ad impedit est et, repellendus architecto adipisci fugit nam laboriosam reprehenderit optio voluptate repudiandae officiis magni nulla sequi. Facere provident consequatur cumque est aspernatur? Illo rem ea incidunt laborum maxime dolore facilis amet iusto quisquam, a beatae, facere error numquam blanditiis inventore molestiae placeat alias eaque, quidem ut. Ipsa.",
+    authorID: 3,
+  },
+  {
+    id: "2",
+    thumbnail: Thumbnail2,
+    category: "science",
+    title: "Blog Post 2",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum distinctio maxime sit ex ad et cumque suscipit mollitia delectus deleniti! Esse at similique modi architecto labore magni maxime non fugiat.",
+    authorID: 1,
+  },
+  {
+    id: "3",
+    thumbnail: Thumbnail3,
+    category: "weather",
+    title: "Blog Post 3",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum distinctio maxime sit ex ad et cumque suscipit mollitia delectus deleniti! Esse at similique modi architecto labore magni maxime non fugiat.",
+    authorID: 13,
+  },
+  {
+    id: "4",
+    thumbnail: Thumbnail4,
+    category: "farming",
+    title: "Blog Post 4",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum distinctio maxime sit ex ad et cumque suscipit mollitia delectus deleniti! Esse at similique modi architecto labore magni maxime non fugiat.",
+    authorID: 11,
+  },
+];
--- client/src/pages/PostDetail.jsx
+++ client/src/pages/PostDetail.jsx
@@ -1,7 +1,91 @@
 import React from "react";
+import PostAuthor from "../components/PostAuthor";
+import { Link } from "react-router-dom";
+import Thumbnail from "../assets/blog22.jpg";
 
 const PostDetail = () => {
-  return <div>PostDetail</div>;
+  return (
+    <section className="post-detail">
+      <div className="container post-detail__container">
+        <div className="post-detail__header">
+          <PostAuthor />
+          <div className="post-detail__buttons">
+            <Link to={`/posts/werwer/edit`} className="btn sm primary">
+              Edit
+            </Link>
+            <Link to={`/posts/werwer/delete`} className="btn sm danger">
+              Delete
+            </Link>
+          </div>
+        </div>
+        <h1>This is the post title!</h1>
+        <div className="post-detail__thumbnail">
+          <img src={Thumbnail} alt="" />
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+          excepturi quos inventore expedita? Fugiat totam at voluptatibus
+          deserunt praesentium quidem saepe consequuntur blanditiis magnam
+          quaerat id iste, ipsa temporibus nisi dolores aliquam excepturi sit.
+          Ducimus unde tempore placeat earum modi.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio,
+          minima id delectus quam aliquam quis dolorem voluptatem at
+          consequuntur distinctio voluptatibus? Sed inventore blanditiis qui
+          quam repudiandae voluptatum at nobis tempore, molestias deserunt ea
+          harum ullam magnam quo? Repellat ab quas deserunt ipsa sed dolores.
+          Reiciendis, exercitationem. Minus blanditiis ullam ipsam? Quam hic
+          eveniet eligendi?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+          reprehenderit magni et. Nam nihil provident vel expedita
+          necessitatibus dolore earum ad. Libero odit ipsum fugiat accusantium.
+          Cupiditate minima, explicabo ullam quos aliquid fuga ea nisi non!
+          Perspiciatis repellat accusamus ullam illum soluta aspernatur nisi cum
+          libero ea nulla hic expedita nemo temporibus inventore, nihil porro
+          tempore voluptatum! Doloribus repudiandae dicta libero quae voluptate
+          iure omnis, reprehenderit architecto magni, incidunt impedit et beatae
+          iusto provident. Distinctio sequi quisquam esse, nulla quas illum,
+          dolorum neque illo dolorem quam, corporis dicta asperiores quos
+          nesciunt numquam atque impedit magnam. Vitae harum doloribus aliquam
+          commodi assumenda fuga, laudantium culpa ratione, excepturi, neque
+          repellat architecto aperiam cupiditate.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+          voluptates animi quaerat facere libero illo aliquid dolor
+          voluptatibus. Quaerat, accusamus.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+          aperiam cupiditate molestiae debitis, libero asperiores corporis
+          voluptatum in nam eaque ab eius facere neque, temporibus pariatur non!
+          Optio odit quisquam at laboriosam ipsam voluptate asperiores eum
+          adipisci, corrupti et dolorem iure aliquam itaque exercitationem atque
+          ducimus natus necessitatibus totam commodi alias. Id maxime
+          dignissimos doloremque quidem, porro eum laborum. Nesciunt optio modi,
+          voluptates sequi perspiciatis, magni, inventore enim quisquam
+          distinctio laboriosam incidunt similique eos quod quia voluptas
+          possimus dolores! Illum temporibus iure quod quis, cupiditate dolore
+          doloribus quam! Sed fugit distinctio illo tempora? Reiciendis soluta
+          ipsum aut assumenda distinctio praesentium consectetur, eligendi culpa
+          vel eum sapiente obcaecati, laborum magni porro explicabo modi,
+          voluptatibus adipisci fugiat. Labore quaerat quam atque possimus
+          mollitia sunt iste voluptate reprehenderit neque voluptas aperiam nam
+          vel temporibus incidunt quos optio, dolorem, adipisci nostrum ab modi
+          dolor eligendi omnis. Incidunt veritatis totam doloribus minus, ipsa
+          laudantium similique voluptatem quas odio non unde temporibus,
+          aspernatur culpa fugiat molestias vel possimus earum, est hic. Tempore
+          est, voluptatem quasi error reprehenderit doloremque eaque nulla
+          perferendis pariatur placeat voluptatum, ea, libero accusantium? Eaque
+          minima dolor pariatur velit et nam reprehenderit! Ut qui ratione illo
+          ex voluptas quia fuga molestias nisi magni.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default PostDetail;
