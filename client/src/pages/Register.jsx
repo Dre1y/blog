a3c10ce7a8af8c640aef0052b69e680e7bf0770c
Andrey <andreymatheus92@gmail.com>
2024-10-26 18:43:39 -0300

    fix: change error message on register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -17,7 +17,7 @@ const Register = () => {
       <div className="container">
         <h2>Sign Up</h2>
         <form action="" className="form register__form">
-          <p className="form__error-message">This is an error message</p>
+          <p className="form__error-message">Fill in all fields.</p>
           <input
             type="text"
             placeholder="Full name"
