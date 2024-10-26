c0117e0748a8a89984ad38f64c361fd709e9e0ed
Andrey <andreymatheus92@gmail.com>
2024-10-26 20:24:05 -0300

    feat: add user profile page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -22,6 +22,7 @@ const Login = () => {
             name="email"
             value={userData.email}
             onChange={changeInputHandler}
+            autoFocus
           />
           <input
             type="password"
